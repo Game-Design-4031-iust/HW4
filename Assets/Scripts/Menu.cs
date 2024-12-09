ee9c49e9944b92c68609930146327689668129bb
Mohammadreza-Edrisabadi <75852598+Mohammadreza-Edrisabadi@users.noreply.github.com>
2024-12-09 21:50:55 +0330

    Initialize game project with core files and assets

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void HandlePlayClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void HandleTryAgainClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void HandleMainMenuClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
